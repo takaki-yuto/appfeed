7b94e5e98784341984275239dbc38c268ac78060
tsueken <tsueken@gmail.com>
2020-09-15 20:48:01 +0900

    user_login, user_signup の実装

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'mysql2', '>= 0.4.4'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
-gem 'sass-rails', '>= 6'
+# gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -29,11 +29,11 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
 end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'pry-rails'
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
@@ -47,6 +47,8 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
@@ -58,3 +60,7 @@ gem 'pagy'
 gem 'carrierwave'
 gem 'redcarpet', '~> 2.3.0'
 gem 'coderay'
+
+gem 'bcrypt'
+gem 'faker'
+gem 'rails-i18n'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,148 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  let(:user) { FactoryBot.build(:user) }
+  describe "name" do
+    context "不正な入力ケース" do
+      example "空白のみの場合" do
+        user.name = "　"
+        expect(user).to be_invalid
+      end
+
+      example "nilの場合" do
+        user.name = nil
+        expect(user).to be_invalid
+      end
+      
+      example "５０文字を超える入力" do
+        user.name = "a" * 51
+        expect(user).to be_invalid
+      end
+
+      example "５０文字の入力" do
+        user.name = "a" * 50
+        expect(user).to be_valid
+      end
+    end
+  end
+  
+  describe "passwordのハッシュ関数への変換の評価" do
+    context "文字列を与える" do
+      example "hashed_passwordは長さ60の文字列になる" do
+        user.password = "appfeed"
+        user.save!
+        expect(user.hashed_password).to be_kind_of(String)
+        expect(user.hashed_password.length).to eq(60)
+      end
+    end
+
+    context "nilを与える" do
+      example "hash_passwordはnilになる" do
+        user = FactoryBot.build(:user, hashed_password: "x")
+        user.password = nil
+        expect(user.hashed_password).to be_nil
+      end
+    end
+  end
+
+  describe "hash_password" do
+    context "適正な入力ケース" do
+      example "６文字以上の文字列である場合" do
+        user.hashed_password = "fooboobar"
+        expect(user).to be_valid
+      end
+    end
+
+    context "不正な入力ケース" do
+      example "blankの場合" do
+        user.hashed_password = " " * 6
+        expect(user).to be_invalid
+      end
+
+      example "6文字未満の場合" do
+        user.hashed_password = "a" * 5
+        expect(user).to be_invalid
+      end
+    end
+  end
+  
+  describe "email" do
+    context "不正な入力ケース" do
+      let(:domein) { "@exapmle.com" }
+
+      example "255文字を超える入力" do
+        user.email = "a" * (256 - domein.length) + domein
+        expect(user.email.length).to eq(256)
+        expect(user).to be_invalid
+      end
+      
+      example "255文字の入力" do
+        user.email = "a" * (255 - domein.length) + domein
+        expect(user.email.length).to eq(255)
+        expect(user).to be_valid
+      end
+
+      example "ドメインに','を含む場合" do
+        user.email = "user@foo,com"
+        expect(user).to be_invalid
+      end
+
+      example "@を含まない入力の場合" do
+        user.email = "user_at_foo.org"
+        expect(user).to be_invalid
+      end
+
+      example "@が2つ含む場合" do
+        user.email = "example.user@foo.foo@bar_baz.com"
+        expect(user).to be_invalid
+      end
+      
+      example "@以下に'+'を含む場合" do
+        user.email = "foo@bar+baz.com "
+        expect(user).to be_invalid
+      end    
+      
+      example "ドメインの'..'を含む場合" do
+        user.email = "foo@bar..com"
+        expect(user).to be_invalid
+      end
+      
+      example "nilの場合" do
+        user.email = nil
+        expect(user).to be_invalid
+      end
+
+      example "空白の場合" do
+        user.email = " "
+        expect(user).to be_invalid
+      end
+    end
+
+    context "emailのupcaseとdowncaseでの入力パターンの評価" do
+      example "emailにupcaseを含む入力した場合" do
+        user.email = "TAKAKIYUTO@gmaiL.COM"
+        down_case_email = user.email.downcase
+        user.save!
+        expect(user.email).to be_kind_of(String)
+        expect(user.reload.email).to eq(down_case_email)
+      end
+    end
+
+    context "emailの一意性の評価" do
+      let(:user2) { FactoryBot.build(:user) }
+      example "異なるアドレスが入力された場合" do
+        user.save!
+        user2.email = "boo@bar.com"
+        expect(user2).to be_valid
+      end
+
+      example "同一のアドレスが入力された場合" do
+        user.email = user2.email = "bar@foo.com"
+        user.save!
+        expect(user2).to be_invalid
+      end
+
+    end
+
+  end
+end 
